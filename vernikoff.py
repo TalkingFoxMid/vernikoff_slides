698f5c7e7f24d32d6c522130f638d90a641ab2e4
TalkingFox <futureinrevolution@gmail.com>
2020-03-20 17:39:34 +0500

    20:03:20

--- vernikoff.py
+++ vernikoff.py
@@ -1,6 +1,9 @@
 import urllib.request
-for i in range(25,32):
-    logo = urllib.request.urlopen("http://kadm.kmath.ru/files/alggeom{0}.pdf".format(i)).read()
-    f = open("alg{0}.pdf".format(i), "wb")
-    f.write(logo)
-    f.close()
+for i in range(25,100):
+    try:
+        logo = urllib.request.urlopen("http://kadm.kmath.ru/files/alggeom{0}.pdf".format(i)).read()
+        f = open("alg{0}.pdf".format(i), "wb")
+        f.write(logo)
+        f.close()
+    except:
+        break
